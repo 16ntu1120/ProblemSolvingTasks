cee87225d83b752e60889e00d04b3600310c6e6a
Muhammad Noman Jelani <noman@Muhammads-MacBook-Pro.local>
2022-11-10 15:46:55 +0500

    Calculating x value in Maths equations

---
+++ equationCalculation.js
@@ -0,0 +1,86 @@
+
+const equation = "3x + 1 = 40";
+const numberRegEx = /^[0-9]+$/;
+let finalResult = 0;
+
+const calculateVariableValue = (eq) => {
+    let variableValue;
+    let firstDigit;
+    let secondDigit;
+    let operator;
+    let obj=[];
+    let digitResult;
+    let splittedVaraible;
+    const convertedEquation = eq.split(' ');
+    for(let i = 0; i < convertedEquation.length; i++) {
+        if(convertedEquation[i].includes('x')) {
+         variableValue = convertedEquation[i];
+        }
+        if(numberRegEx.test(convertedEquation[i])){
+            obj.push(parseInt(convertedEquation[i]))
+        }
+        if(convertedEquation[i] === '+' || convertedEquation[i] === '-' || convertedEquation[i] === '*'|| convertedEquation[i] === '/') {
+            operator = convertedEquation[i];
+        }
+    }
+    if(obj.length) {
+        for(let i = 0; i < obj.length; i++) {
+            if(i == 0) {
+                firstDigit = obj[i];
+            } else {
+                secondDigit = obj[i];
+            }
+        }
+    }
+    if(firstDigit > secondDigit) {
+       if(operator == '+') {
+           if(convertedEquation.indexOf('+') == 1) {
+               digitResult = firstDigit - secondDigit;
+           } else {
+               digitResult = firstDigit + secondDigit;
+           }
+       }
+    }
+    if(secondDigit > firstDigit) {
+         if(operator == '+') {
+             if(convertedEquation.indexOf('+') == 1) {
+                 digitResult = secondDigit - firstDigit;
+             } else {
+                  digitResult = secondDigit + firstDigit;
+             }
+       }
+    }
+    const val = splitVariableString(variableValue);
+    if(numberRegEx.test(val)) {
+        finalResult = digitResult / val;
+        return finalResult;
+    } else {
+        return digitResult;
+    }
+}
+
+const splitVariableString = (str) => {
+    if(str.length > 1) {
+        const variableSpliting = str.split("");
+        for (let i = 0; i < variableSpliting.length; i++) {
+            const separatingVariable = numberRegEx.test(variableSpliting[i]);
+            if (separatingVariable == true) {
+                return variableSpliting[i];
+            } 
+        }
+    } else {
+        return str;
+    }
+}
+
+
+const res = calculateVariableValue(equation);
+console.log(res);
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
